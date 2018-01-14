e63cfcf8af73cc7cfe62c4f31d76cdb2ad645b9a
Marcin Bigos <marcin_99999@interia.pl>
2018-01-14 22:20:28 +0100

    Create API project, and connect this layer to infrastructure (basic connection)

---
+++ Hated.Api/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+using Hated.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hated.Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Users")]
+    public class UsersController : Controller
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET api/users
+        [HttpGet("{email}")]
+        public async Task<IActionResult> GetAsync(string email)
+        {
+            var user = await _userService.GetAsync(email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Json(user);
+        }
+    }
+}
