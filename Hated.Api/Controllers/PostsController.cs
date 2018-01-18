d2dfd0e28bb3651c91b9447d96a463a0e709d90f
Marcin Bigos <marcin_99999@interia.pl>
2018-01-18 18:55:51 +0100

    Create PostsController

---
+++ Hated.Api/Controllers/PostsController.cs
@@ -0,0 +1,15 @@
+﻿using Hated.Infrastructure.Services;
+
+namespace Hated.Api.Controllers
+{
+    public class PostsController : BaseController
+    {
+        private readonly IPostService _postService;
+
+        public PostsController(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+    }
+}
