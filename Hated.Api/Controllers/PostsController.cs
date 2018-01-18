ebc15189eb84891f784c314d3628e101f006e4e4
Marcin Bigos <marcin_99999@interia.pl>
2018-01-18 19:26:25 +0100

    Create PostsController with CRUD like UsersController

--- Hated.Api/Controllers/PostsController.cs
+++ Hated.Api/Controllers/PostsController.cs
@@ -1,4 +1,9 @@
-﻿using Hated.Infrastructure.Services;
+﻿using System;
+using System.Threading.Tasks;
+using Hated.Infrastructure.Commands.Posts;
+using Hated.Infrastructure.DTO;
+using Hated.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hated.Api.Controllers
 {
@@ -11,5 +16,58 @@ namespace Hated.Api.Controllers
             _postService = postService;
         }
 
+        //Create
+        //POST api/posts
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromBody] CreatePost newPost)
+        {
+            var postId = await _postService.AddAsync(newPost.UserId, newPost.Content);
+            return Created($"api/posts/{postId}", null);
+        }
+
+        //Read
+        //GET api/posts/guid
+        [HttpGet("postId")]
+        public async Task<IActionResult> GetAsync(Guid postId)
+        {
+            var post = await _postService.GetAsync(postId);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return Json(post);
+        }
+
+        //GET api/posts
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var posts = await _postService.GetAllAsync();
+            if (posts == null)
+            {
+                return NotFound();
+            }
+
+            return Json(posts);
+        }
+
+        //Update
+        //PUT api/posts
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody]PostDto updatedPost)
+        {
+            await _postService.UpdateAsync(updatedPost);
+            return Ok();
+        }
+
+        //Delete
+        //DELETE api/posts/guid
+        [HttpDelete("{postId}")]
+        public async Task<IActionResult> DeleteAsync(Guid postId)
+        {
+            await _postService.DeleteAsync(postId);
+            return Ok();
+        }
     }
 }
