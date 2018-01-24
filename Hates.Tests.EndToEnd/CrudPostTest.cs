a30f3a18a95c510c840bbdc67a5d5e5a3ecf57f3
Marcin Bigos <marcin_99999@interia.pl>
2018-01-24 20:23:06 +0100

    Test CRUD of posts

---
+++ Hates.Tests.EndToEnd/CrudPostTest.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hates.Tests.EndToEnd
+{
+    public class CrudPostTest : BaseMethodsToTests
+    {
+        [Fact]
+        public async Task CreatedPostShouldBeCreated()
+        {
+            var user = await CreateAndGetRandomUser();
+            var response = await CreateNewPost(user.Id);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task CreatedPostShouldBeValid()
+        {
+            var user = await CreateAndGetRandomUser();
+            string content = Guid.NewGuid().ToString();
+            var response = await CreateNewPost(user.Id, content);
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+            var post = await GetPostAsync(response.Headers.Location.ToString());
+            Assert.Equal(user.Id, post.UserId);
+            Assert.Equal(content, post.Content);
+        }
+
+        [Fact]
+        public async Task UpdatedPostShouldBeUpdated()
+        {
+            var user = await CreateAndGetRandomUser();
+            var response = await CreateNewPost(user.Id);
+            var post = await GetPostAsync(response.Headers.Location.ToString());
+            string updatedPostContent = Guid.NewGuid().ToString();
+            post.Content = updatedPostContent;
+            var payload = GetPayload(post);
+            await Client.PutAsync("api/posts", payload);
+            var updatedPost = await GetPostAsync(response.Headers.Location.ToString());
+            Assert.Equal(updatedPostContent, updatedPost.Content);
+
+        }
+
+        [Fact]
+        public async Task DeletedPostShouldBeDeleted()
+        {
+            var user = await CreateAndGetRandomUser();
+            var response = await CreateNewPost(user.Id);
+            var responseDeleted = await Client.DeleteAsync(response.Headers.Location.ToString());
+            Assert.Equal(HttpStatusCode.OK, responseDeleted.StatusCode);
+        }
+    }
+}
--- Hates.Tests.EndToEnd/CrudUserTest.cs
+++ Hates.Tests.EndToEnd/CrudUserTest.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Hated.Infrastructure.Commands.Users;
 using Hated.Infrastructure.DTO;
 using Newtonsoft.Json;
 using Xunit;
@@ -13,14 +11,6 @@ namespace Hates.Tests.EndToEnd
 {
     public class CrudUserTest : BaseMethodsToTests
     {
-        [Fact]
-        public async Task ValidUserShouldBeReturned()
-        {
-            string emailTestedUser = "user1@email.com";
-            var user = await GetUserAsync(emailTestedUser);
-            Assert.Equal(emailTestedUser, user.Email);
-        }
-        
         [Fact]
         public async Task GetAllUsersShouldReturnListOfUsers()
         {
@@ -50,29 +40,22 @@ namespace Hates.Tests.EndToEnd
         [Fact]
         public async Task UpdatedUserShouldBeUpdated()
         {
-            string emailTestedUser = Guid.NewGuid().ToString();
-            await CreateNewUser(emailTestedUser);
-
-            var user = await GetUserAsync(emailTestedUser);
-            string usernameAfterUpdate = "updated";
+            var user = await CreateAndGetRandomUser();
+            string usernameAfterUpdate = Guid.NewGuid().ToString();
             user.Username = usernameAfterUpdate;
             
             var payload = GetPayload(user);
             var response = await Client.PutAsync("api/users", payload);
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
 
-            var updatedUser = await GetUserAsync(emailTestedUser);
+            var updatedUser = await GetUserAsync(user.Email);
             Assert.Equal(usernameAfterUpdate, updatedUser.Username);
         }
 
         [Fact]
         public async Task DeletedUserShouldBeDelete()
         {
-            string emailTestedUser = Guid.NewGuid().ToString();
-            var responseCreated = await CreateNewUser(emailTestedUser);
-            Assert.Equal(HttpStatusCode.Created, responseCreated.StatusCode);
-
-            var user = await GetUserAsync(emailTestedUser);
+            var user = await CreateAndGetRandomUser();
             
             var responseDeleted = await Client.DeleteAsync($"api/users/{user.Id}");
 
