5d1f48c6e7f00190ca07680c97ccbbcb47e5adec
Marcin Bigos <marcin_99999@interia.pl>
2018-02-03 23:59:32 +0100

    Implement custom IoC

---
+++ Hated.Infrastructure/IoC/Modules/RepositoryModule.cs
@@ -0,0 +1,21 @@
+﻿using Autofac;
+using System.Reflection;
+using Hated.Core.Repositories;
+
+namespace Hated.Infrastructure.IoC.Modules
+{
+    public class RepositoryModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var assembly = typeof(RepositoryModule)
+                .GetTypeInfo()
+                .Assembly;
+
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(x => x.IsAssignableTo<IRepository>())
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
