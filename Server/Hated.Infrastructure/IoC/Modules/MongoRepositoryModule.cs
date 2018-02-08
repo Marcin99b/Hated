d1cbed5473a68e4e9e5ab7b35bd159ab1a58116b
Marcin Bigos <marcin_99999@interia.pl>
2018-02-08 22:28:01 +0100

    Add mongo repository module

---
+++ Server/Hated.Infrastructure/IoC/Modules/MongoRepositoryModule.cs
@@ -0,0 +1,38 @@
+﻿using Autofac;
+using System.Reflection;
+using Hated.Core.Repositories;
+using Hated.Infrastructure.Mongo;
+using MongoDB.Driver;
+
+namespace Hated.Infrastructure.IoC.Modules
+{
+    public class MongoRepositoryModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.Register((c, p) =>
+            {
+                var settings = c.Resolve<MongoSettings>();
+                return new MongoClient(settings.ConnectionString);
+            }).SingleInstance();
+
+            builder.Register((c, p) =>
+            {
+                var client = c.Resolve<MongoClient>();
+                var settings = c.Resolve<MongoSettings>();
+                var database = client.GetDatabase(settings.Database);
+
+                return database;
+            }).As<IMongoDatabase>();
+
+            var assembly = typeof(MongoRepositoryModule)
+                .GetTypeInfo()
+                .Assembly;
+
+            builder.RegisterAssemblyTypes(assembly)
+                .Where(x => x.IsAssignableTo<IRepository>())
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+        }
+    }
+}
