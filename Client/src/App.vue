c599ac94d9be237607ca6ada40e56a856411422c
Stepek123 <stepek75@gmail.com>
2018-02-08 22:25:44 +0100

    Commit

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,15 +1,8 @@
 <template>
   <div id="app">
-      <router-view></router-view>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-export default {
-
-};
-</script>
-
 <style>
 body {
   margin: 0;
--- Client/src/router/index.js
+++ Client/src/router/index.js
@@ -5,7 +5,6 @@ import Hello from '@/components/Hello';
 Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
   routes: [
     {
       path: '/',
