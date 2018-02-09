86076205eb6b8de6f21d757d85ed5a07295124fc
Marcin Bigos <marcin_99999@interia.pl>
2018-02-09 22:21:46 +0100

    Allow any CORS

--- Backend/Hated.Api/Controllers/BaseController.cs
+++ Backend/Hated.Api/Controllers/BaseController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Hated.Api.Controllers
 {
     [Produces("application/json")]
     [Route("[controller]")]
+    [EnableCors("AllowAny")]
     public abstract class BaseController : Controller
     {
         protected BaseController()
