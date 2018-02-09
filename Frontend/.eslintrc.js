998ebc276e255a5cbc00a2c285a087d5342205c7
Stepek123 <stepek75@gmail.com>
2018-02-09 17:42:14 +0100

    Naprawiony błąd z Eslint

--- Frontend/.eslintrc.js
+++ Frontend/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
+    'linebreak-style': 0,
     // don't require .vue extension when importing
     'import/extensions': ['error', 'always', {
       'js': 'never',
